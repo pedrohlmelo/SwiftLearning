e12f282c6f7ee830e54c927a14dd7574d906f6dd
Pedro Henrique Lopes De Melo <pedrolopesmelo18@gmail.com>
2026-05-17 11:01:55 -0300

    add logica

---
+++ Logica.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+let result = 2 > 3
+ 
+let x = 10
+let y = 20
+let res = x != y
+//print(res) -> true
+
+
+let firstName = "Pedro"
+let lastName = "Lopes"
+
+let resul = firstName > lastName
+//print(resul) -> true
+
+let isDriver = false
+let isStudent = true
+
+//print(!isDriver) -> true
+
+let r = isDriver || isStudent
+//print(r) -> true
+
+let a = isDriver && isStudent
+//print(a) -> false
+
+
