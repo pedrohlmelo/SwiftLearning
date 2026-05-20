7cdd42c2a46eaf14167bd04909353f6c03a8b760
Pedro Henrique Lopes De Melo <pedrolopesmelo18@gmail.com>
2026-05-20 20:57:42 -0300

    add Tuplas

---
+++ Tuplas.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+let coordenadas: (Double, Double) = (20.4, 55.2)
+//print(coordenadas.0)
+//print(coordenadas.1)
+
+let coords =  (lat: 20.4, lng: 55.2)
+//print(coords.lat)
+//print(coords.lng)
+
+let camera = (x: 15, y: 25, z: 10)
+let(x, y, z) = camera
+print(x)
+print(y)
+print(z)
+
+let user = (name: "Pedro",age: "19")
+print(user.name)
+print(user.age)
+
+
+
