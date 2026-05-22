6a7a58521c90eb4c1e5612a9854cb29ff3679da3
Pedro Henrique Lopes De Melo <pedrolopesmelo18@gmail.com>
2026-05-21 22:42:05 -0300

    add arrays

---
+++ Arrays.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+//let userNames : [String] = ["Pedro", "Lopes", "Melo"]
+//print(userNames)
+
+var userNames : [String] = []
+userNames.append("Pedro")
+userNames += ["Lopes", "Melo"]
+print(userNames)
