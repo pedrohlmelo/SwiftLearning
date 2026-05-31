6cba37675ff43a4f0b0c7c37605360e86d6eedf4
Pedro Henrique Lopes De Melo <pedrolopesmelo18@gmail.com>
2026-05-31 13:41:12 -0300

    add Repeat e While

---
+++ RepeatWhile.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var i = 1
+/*while i < 10 {
+    print(i)
+    i+=1
+}*/
+
+// com o repeat ele primeiro faz a instrucao e depois checa a condicao (como se fosse um do-while)
+
+repeat{
+    print(i)
+    i+=1
+}while i < 10
